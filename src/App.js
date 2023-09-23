bbbf8ba5dd888f5b61b513409b2b33eb6cf25987
Emmanuel Jose Ingua <emmanueljoseingua@gmail.com>
2023-09-23 16:34:00 +0800

    created sidebar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import Main from './pages/Main';
 
 class App extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component = {Login} />
                 <Route path="/login" component = {Login}  />
                 <Route path="/register" component = {Register}  />
-                <Route path="/dashboard" component = {Dashboard} />
+                <Route path="/main" component = {Main} />
             </Switch>
         );
     }
 }
+
 
 export default App;
