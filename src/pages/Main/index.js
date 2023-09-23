b1bcc8a152ce1825963aa9cbca3ec16dae6eed95
Tinapay <tinapaylance@gmail.com>
2023-09-23 17:27:30 +0800

    LANCE

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,126 +1,84 @@
-import React, { Component } from 'react';
-
-class Main extends Component {
-    render() {
-        return React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'div',
-                { className: 'd-flex flex-column flex-shrink-0 p-3 text-bg-dark', style: { width: '280px', height: '100vh' } },
-                React.createElement(
-                    'a',
-                    { href: '/', className: 'd-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none' },
-                    React.createElement(
-                        'svg',
-                        { className: 'bi pe-none me-2', width: '40', height: '32' },
-                        React.createElement('use', { xlinkHref: '#bootstrap' })
-                    ),
-                    React.createElement('span', { className: 'fs-4' }, 'Sidebar')
-                ),
-                React.createElement('hr', null),
-                React.createElement(
-                    'ul',
-                    { className: 'nav nav-pills flex-column mb-auto' },
-                    React.createElement(
-                        'li',
-                        { className: 'nav-item' },
-                        React.createElement(
-                            'a',
-                            { href: '#', className: 'nav-link active', 'aria-current': 'page' },
-                            React.createElement(
-                                'svg',
-                                { className: 'bi pe-none me-2', width: '16', height: '16' },
-                                React.createElement('use', { xlinkHref: '#home' })
-                            ),
-                            'Home'
-                        )
-                    ),
-                    React.createElement(
-                        'li',
-                        null,
-                        React.createElement(
-                            'a',
-                            { href: '#', className: 'nav-link text-white' },
-                            React.createElement(
-                                'svg',
-                                { className: 'bi pe-none me-2', width: '16', height: '16' },
-                                React.createElement('use', { xlinkHref: '#speedometer2' })
-                            ),
-                            'Dashboard'
-                        )
-                    ),
-                    React.createElement(
-                        'li',
-                        null,
-                        React.createElement(
-                            'a',
-                            { href: '#', className: 'nav-link text-white' },
-                            React.createElement(
-                                'svg',
-                                { className: 'bi pe-none me-2', width: '16', height: '16' },
-                                React.createElement('use', { xlinkHref: '#table' })
-                            ),
-                            'Orders'
-                        )
-                    ),
-                    React.createElement(
-                        'li',
-                        null,
-                        React.createElement(
-                            'a',
-                            { href: '#', className: 'nav-link text-white' },
-                            React.createElement(
-                                'svg',
-                                { className: 'bi pe-none me-2', width: '16', height: '16' },
-                                React.createElement('use', { xlinkHref: '#grid' })
-                            ),
-                            'Products'
-                        )
-                    ),
-                    React.createElement(
-                        'li',
-                        null,
-                        React.createElement(
-                            'a',    
-                            { href: '#', className: 'nav-link text-white' },
-                            React.createElement(
-                                'svg',
-                                { className: 'bi pe-none me-2', width: '16', height: '16' },
-                                React.createElement('use', { xlinkHref: '#people-circle' })
-                            ),
-                            'Customers'
-                        )
-                    )
-                ),
-                React.createElement('hr', null),
-                React.createElement(
-                    'div',
-                    { className: 'dropdown' },
-                    React.createElement(
-                        'a',
-                        {
-                            href: '#',
-                            className: 'd-flex align-items-center text-white text-decoration-none dropdown-toggle',
-                            'data-bs-toggle': 'dropdown',
-                            'aria-expanded': 'false',
-                        },
-                        React.createElement('img', { src: 'https://github.com/mdo.png', alt: '', width: '32', height: '32', className: 'rounded-circle me-2' }),
-                        React.createElement('strong', null, 'mdo')
-                    ),
-                    React.createElement(
-                        'ul',
-                        { className: 'dropdown-menu dropdown-menu-dark text-small shadow' },
-                        React.createElement('li', null, React.createElement('a', { className: 'dropdown-item', href: '#' }, 'New project...')),
-                        React.createElement('li', null, React.createElement('a', { className: 'dropdown-item', href: '#' }, 'Settings')),
-                        React.createElement('li', null, React.createElement('a', { className: 'dropdown-item', href: '#' }, 'Profile')),
-                        React.createElement('li', null, React.createElement('hr', { className: 'dropdown-divider' })),
-                        React.createElement('li', null, React.createElement('a', { className: 'dropdown-item', href: '#' }, 'Sign out'))
-                    )
-                )
-            )
-        );
-    }
+import React, { Component } from 'react'
+import {Link, Switch, Route} from 'react-router-dom'
+import Dashboard from '../Dashboard'
+export default class Main extends Component {
+  state = { activePage: '' }
+  componentDidMount = () => {
+    this.setState({
+      activePage: 'dashboard'
+    })
+  }
+  handleClickSidebar = (pages) => {
+    this.setState({
+      activePage: pages
+    })
+  }
+  render() {
+    return (
+      <div class="row">
+      <div class="col-3 text-white bg-dark" style={{height: '100vh'}}>
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <span class="fs-4">Sidebar</span>
+        </a>
+        <hr/>
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <Link to="/home" onClick={() => this.handleClickSidebar('home')}>
+            <div class={this.state.activePage === 'home' ? "nav-link active" : "text-white nav-link"} aria-current="page">
+              Home
+            </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard" onClick={() => this.handleClickSidebar('dashboard')}>
+            <div class={this.state.activePage === 'dashboard' ? "nav-link active" : "text-white nav-link"}>
+              Dashboard
+            </div>
+            </Link>
+          </li>
+          <li>
+          <Link to="/Orders" onClick={() => this.handleClickSidebar('Orders')}>
+            <div class={this.state.activePage === 'Orders' ? "nav-link active" : "text-white nav-link"}>
+              Orders
+            </div>
+            </Link>
+          </li>
+          <li>
+          <Link to="/Products" onClick={() => this.handleClickSidebar('Products')}>
+            <div class={this.state.activePage === 'Products' ? "nav-link active" : "text-white nav-link"}>
+              Products
+            </div>
+            </Link>
+          </li>
+          <li>
+          <Link to="/Customer" onClick={() => this.handleClickSidebar('Customer')}>
+            <div class={this.state.activePage === 'Customer' ? "nav-link active" : "text-white nav-link"}>
+              Customer
+            </div>
+            </Link>
+          </li>
+        </ul>
+        <hr/>
+        <div class="dropdown">
+          <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/>
+            <strong>mdo</strong>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-9">
+          <Switch>
+              <Route path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default Main;
