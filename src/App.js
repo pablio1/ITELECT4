e1fb36ce137466238bab73dcabca8b743e7753a5
Rivera <billy100007@gmail.com>
2023-09-23 17:09:59 +0800

    2nd attempt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import main from "./pages/main";
-
+import "./styles/main.css";
 class App extends Component {
     render() {
         return (
