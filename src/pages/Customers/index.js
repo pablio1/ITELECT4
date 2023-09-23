185f14c1c1edbb7b0fce0f3618f6e6dc1085668a
IanEarle <ianjimenezt@gmail.com>
2023-09-23 17:27:05 +0800

    Sidebar Act

---
+++ src/pages/Customers/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                Customers page
+            </div>
+        );
+    }
+}
+
+export default index;
