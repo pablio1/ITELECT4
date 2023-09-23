2382281f5742cfcc965cb1281b827e5b9ce6c1d7
John Victor Ong <mijakolgaming.com>
2023-09-23 16:24:05 +0800

    9/23/23

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import Main from './pages/Main';
 
 class App extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component = {Login} />
                 <Route path="/login" component = {Login}  />
                 <Route path="/register" component = {Register}  />
-                <Route path="/dashboard" component = {Dashboard} />
+                <Route path="/Main" component = {Main} />
             </Switch>
         );
     }
