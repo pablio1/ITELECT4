e385767b83be9c9bbcb0370f92761938210ea277
Rivera <billy100007@gmail.com>
2023-09-23 17:15:27 +0800

    3rd attempt sept 23 2023

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import main from "./pages/main";
-import "./styles/main.css";
+import Main from './pages/Main';
+
 class App extends Component {
     render() {
         return (
             <Switch>
-               
                 <Route path="/login" component = {Login}  />
                 <Route path="/register" component = {Register}  />
-                <Route path="/main" component = {main} />
-   
+                <Route path="/" component = {Main} />
             </Switch>
         );
     }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,20 +4,7 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-                <title>DASHBOARD PAGE</title>
-                    <center><h1>DASHBOARD PAGE</h1></center> 
-                    <center><a href='/Login' target='blank'><button className="btn btn-secondary"><h1 className='address'>LOG IN PAGE</h1></button></a> </center> 
-
-                <div>
-            <center><h1>HEADER</h1></center>  
-            <div className='main'>
-                    <div className='left'><h1>left</h1></div> 
-                    <div className='right'><h1>RIGHT</h1></div> 
-                    <div className='middle'><h1>middle</h1></div>
-            </div>
-            <center><div className='bottom'><h1>bottom</h1></div></center>   
-            
-        </div>
+                Welcome User!
             </div>
         );
     }
