16b1d2ef09de8cbcc967590bdb82375d72ca2e3a
Juntilla <cljuntilla91@gmail.com>
2023-09-23 18:37:52 +0800

    with color for fun

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -30,7 +30,7 @@ export default class Main extends Component {
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
       <Link to="/home" onClick={() => this.handleClickSidebar('home')} Link style={{ color: 'inherit', textDecoration: 'inherit'}}>
-        <div class={this.state.activePage === 'home' ? "nav-link active" : "nav-link" } style={{color:'green'}} aria-current="page">
+        <div class={this.state.activePage === 'home' ? "nav-link active" : "nav-link" } style={{color:'yellow'}} aria-current="page">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16" style={{marginRight: '5px', marginBottom: '5px'}}>
   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
   <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
