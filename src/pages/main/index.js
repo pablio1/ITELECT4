0dff6a077643d47cff7bea801a5490d76d9e255e
Rivera <billy100007@gmail.com>
2023-09-23 18:32:17 +0800

    with complete icons

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -41,21 +41,29 @@ class Main extends Component {
                     <li>
                         <Link to="/dashboard"  onClick={() => this.handleClickSidebar('dashboard')}Link style={{color:'inherit',textDecoration:'inherit'}}>
                             <div class={this.state.activePage === 'dashboard' ? "nav-link active" : "text-white nav-link" } >
-                            Dashboard
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16" style={{marginRight: '5px', marginBottom:'5px'}}>
+  <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+  <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+</svg>Dashboard
                             </div>
                         </Link>
                     </li>
                     <li>
                         <Link to="/orders"onClick={() => this.handleClickSidebar('Orders')} Link style={{color:'inherit',textDecoration:'inherit'}}>
                         <div class={this.state.activePage === 'Orders' ? "nav-link active" : "text-white nav-link" } >
-                            Orders
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-raised-hand" viewBox="0 0 16 16" style={{marginRight: '5px', marginBottom:'5px'}}>
+  <path d="M6 6.207v9.043a.75.75 0 0 0 1.5 0V10.5a.5.5 0 0 1 1 0v4.75a.75.75 0 0 0 1.5 0v-8.5a.25.25 0 1 1 .5 0v2.5a.75.75 0 0 0 1.5 0V6.5a3 3 0 0 0-3-3H6.236a.998.998 0 0 1-.447-.106l-.33-.165A.83.83 0 0 1 5 2.488V.75a.75.75 0 0 0-1.5 0v2.083c0 .715.404 1.37 1.044 1.689L5.5 5c.32.32.5.754.5 1.207Z"/>
+  <path d="M8 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"/>
+</svg>Orders
                             </div>
                         </Link>
                     </li>
                     <li>
                         <Link to="/products"onClick={() => this.handleClickSidebar('Products')}Link style={{color:'inherit',textDecoration:'inherit'}}>
                         <div class={this.state.activePage === 'Products' ? "nav-link active" : "text-white nav-link" } >
-                            Products
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box2" viewBox="0 0 16 16" style={{marginRight: '5px', marginBottom:'5px'}}>
+  <path d="M2.95.4a1 1 0 0 1 .8-.4h8.5a1 1 0 0 1 .8.4l2.85 3.8a.5.5 0 0 1 .1.3V15a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4.5a.5.5 0 0 1 .1-.3L2.95.4ZM7.5 1H3.75L1.5 4h6V1Zm1 0v3h6l-2.25-3H8.5ZM15 5H1v10h14V5Z"/>
+</svg>Products
                             </div>
                         </Link>
                     </li>
