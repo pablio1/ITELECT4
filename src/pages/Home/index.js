2ca58018217aa62e40d1ca8b7d1e7d8ca1238f75
queennie26 <queenniearo.265@gmail.com>
2023-10-12 00:55:04 +0800

    search bar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,76 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>home</h1>
-        Home
+      
+      <div style={{background:'https://storage.pixteller.com/designs/designs-images/2019-03-27/05/simple-background-backgrounds-passion-simple-1-5c9b95c3a34f9.png'}}>
+        <body>
+
+          
+
+          <div class="jumbotron text-center">
+            <h1>Neflix</h1>
+            <h3>All movies are for free !!!!</h3>
+          </div>
+
+          <div class="container">
+            <form action="/action_page.php">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" name="search" />
+                <div class="input-group-btn">
+                  <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <br></br>
+
+          
+
+          <hr></hr>
+
+
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+              <div class="col-sm-2">
+                <h2>Thumbnail</h2>
+                <img src="https://i.pinimg.com/originals/55/9d/bb/559dbb42ab8c1569dfe890d9b40ce2dc.jpg" class="img-thumbnail" alt="Cinque Terre" width="200" height="200" />
+                <h3>The movie</h3>
+                <p>genre: Action, Romance</p>
+              </div>
+            </div>
+          </div>
+
+        </body>
 
       </div>
     )
