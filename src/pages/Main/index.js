b1a9df8873df2931fc07432575a3590fe9f132cb
Jessa Rose Tepace <jessarosetepace24@gmail.com>
2023-10-11 21:14:52 +0800

    ui for movie website

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,79 +1,83 @@
 import React, { Component } from 'react';
 import {Link, Switch, Route} from 'react-router-dom';
-import Dashboard from '../Dashboard';
+
 class Main extends Component {
-    state = { activePage: '' }
-    componentDidMount = () => {
-        this.setState({
-            activePage: 'dashboard'
-        })
-    }
-
-    handleClickSidebar = (page) => {
-        this.setState({
-            activePage: page
-        })
-    }
-
     render() {
         return (
-            <div class="row">
-                <div class="col-3 text-white bg-dark" style={{ height: '100vh'}}>
-                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    
-                    <span class="fs-4">Sidebar</span>
-                    </a>
-                    <hr/>
-                    <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <Link to="/home" onClick={() => this.handleClickSidebar('home')}>
-                            <div class={this.state.activePage === 'home' ? "nav-link active" : "nav-link"} aria-current="page">
-                            Home
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard"  onClick={() => this.handleClickSidebar('dashboard')}>
-                            <div class={this.state.activePage === 'dashboard' ? "nav-link active" : "nav-link"} >
-                            Dashboard
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                    <Link to="/orders"  onClick={() => this.handleClickSidebar('orders')}>
-                            <div class={this.state.activePage === 'orders' ? "nav-link active" : "nav-link"} aria-current="page">
-                            Orders
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                    <Link to="/products"  onClick={() => this.handleClickSidebar('products')}>
-                            <div class={this.state.activePage === 'products' ? "nav-link active" : "nav-link"} aria-current="page">
-                            Products
-                            </div>
-                        </Link>
-                    </li>
-                    </ul>
-                    <hr/>
-                    <div class="dropdown">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/>
-                        <strong>mdo</strong>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><Link><div class="dropdown-item" href="#">New project...</div></Link></li>
-                        <li><Link><div class="dropdown-item" href="#">Settings</div></Link></li>
-                        <li><Link><div class="dropdown-item" href="#">Profile</div></Link></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><Link><div class="dropdown-item" href="#">Sign out</div></Link></li>
-                    </ul>
+            <div >
+                <div class="container-fluid">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+                        <div class="col-md-3 mb-2 mb-md-0">
+                            <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+        
+                            </a>
+                        </div>
+
+                        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                            <li><Link to="/"><div class="nav-link px-2 link-secondary">Home</div></Link></li>
+                            <li><Link to="/"><div class="nav-link px-2 link-secondary">Genre</div></Link></li>
+                            <li><Link to="/"><div class="nav-link px-2 link-secondary">Country</div></Link></li>
+                            <li><Link to="/"><div class="nav-link px-2 link-secondary">A-Z List</div></Link></li>
+                            <li><Link to="/"><div class="nav-link px-2 link-secondary">Search</div></Link></li>
+                        </ul>
+
+                        <div class="col-md-3 text-end">
+                            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+                            <button type="button" class="btn btn-primary">Sign-up</button>
+                        </div>
+                        
                     </div>
                 </div>
-                <div class="col-9">
-                    <Switch>
-                        <Route path="/dashboard" component={Dashboard} />
-                    </Switch>
+                <div class="container-fluid">
+                    <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+                        </div>
+                        <div class="carousel-inner">
+                        <div class="carousel-item active">
+                                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                                <div class="container">
+                                    <div class="carousel-caption">
+                                        <h1>Movie Title</h1>
+                                        <p>Movie description</p>
+                                        <p><a class="btn btn-lg btn-primary" href="#">Watch Trailer</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                                <div class="container">
+                                    <div class="carousel-caption">
+                                        <h1>Movie Title</h1>
+                                        <p>Movie description</p>
+                                        <p><a class="btn btn-lg btn-primary" href="#">Watch Trailer</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item active">
+                                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                                <div class="container">
+                                    <div class="carousel-caption">
+                                        <h1>Movie Title</h1>
+                                        <p>Movie description</p>
+                                        <p><a class="btn btn-lg btn-primary" href="#">Watch Trailer</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                    </div>
                 </div>
+                
             </div>
         );
     }
