bd4b1e65ab718ea677e0bfe4114fdf3928039a29
Mary Ann Inoc <maryanninoc2190@gmail.com>
2023-10-13 15:30:20 +0800

    SearchMovies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,134 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main';
+import styled from 'styled-components';
+import Axios from "axios";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
 
-class App extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route path="/" component = {Main}  />
-                <Route path="/register" component = {Register}  />
-                <Route path="/" component = {Main} />
-            </Switch>
-        );
-    }
+import './App.css';
+export const API_KEY = "942b8668";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+const Placeholder = styled.img`
+  width: 2000px;
+  height: 750px;
+  margin: 50px;
+  opacity: 300%;
+`;
+
+function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+  return (
+    <Container>
+      <Header>
+        <AppName>
+          <MovieImage src="/react-movie-app/movie-icon.svg" />
+          ITELECT4 MARY ANN INOC
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/react-movie-app/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
+      <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+            
+          <Placeholder src="/react-movie-app/harry-potter-8-film-digital-posters.webp" />
+          
+        )}
+      </MovieListContainer>
+    </Container>
+  );
 }
 
 export default App;
