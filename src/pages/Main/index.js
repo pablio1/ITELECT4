c409a73749020a90957e2732d1dd18f2e48a8c72
Aying,-John-Neil <ayingneil15@gmail.com>
2023-10-14 10:21:50 +0800

    main ui

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,81 +1,72 @@
 import React, { Component } from 'react';
-import {Link, Switch, Route} from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard';
 import Home from '../Home';
 
+// Import your CSS file
+import './styles.css';
+
 class Main extends Component {
     state = { activePage: '' }
+
     componentDidMount = () => {
         this.setState({
-            activePage: 'dashboard'
+            activePage: 'home'
         })
     }
 
-    handleClickSidebar = (page) => {
+    handleClickNav = (page) => {
         this.setState({
             activePage: page
         })
     }
 
+    handleSearch = () => {
+        // Handle the movie search here using the YouTube API
+        // You can make an API request to search for movies based on user input
+    }
+
     render() {
+        const navLinkStyle = {
+            color: 'white',
+            fontSize: '18px', // You can adjust the font size as needed
+            textDecoration: 'none', // Remove underlines
+        };
+
+        const backgroundStyle = {
+            backgroundImage: `url('https://img.redbull.com/images/w_1200/q_auto,f_auto/redbullcom/2014/10/09/1331683687434_2/dota-2-is-unlike-most-games-of-its-kind')`,
+            backgroundSize: 'cover',
+            minHeight: '100vh',
+            backgroundRepeat: 'no-repeat',
+        };
+
         return (
-            <div class="row">
-                <div class="col-3 text-white bg-dark" style={{ height: '100vh'}}>
-                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    
-                    <span class="fs-4">Sidebar</span>
-                    </a>
-                    <hr/>
-                    <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <Link to="/home" onClick={() => this.handleClickSidebar('home')}>
-                            <div class={this.state.activePage === 'home' ? "nav-link active" : "nav-link"} aria-current="page">
+            <div style={backgroundStyle}>
+                {/* Top Navigation Bar */}
+                <nav className="navbar navbar-dark bg-dark">
+                    <div className="container">
+                        <Link to="/" className="navbar-brand" onClick={() => this.handleClickNav('home')}>
+                            Dota Movie
+                        </Link>
+                        <Link to="/home" className={this.state.activePage === 'home' ? "nav-link active" : "nav-link"} style={{ ...navLinkStyle, marginRight: '10px' }} onClick={() => this.handleClickNav('home')}>
                             Home
-                            </div>
                         </Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard"  onClick={() => this.handleClickSidebar('dashboard')}>
-                            <div class={this.state.activePage === 'dashboard' ? "nav-link active" : "nav-link"} >
+                        <Link to="/dashboard" className={this.state.activePage === 'dashboard' ? "nav-link active" : "nav-link"} style={{ ...navLinkStyle, marginRight: '10px' }} onClick={() => this.handleClickNav('dashboard')}>
                             Dashboard
-                            </div>
                         </Link>
-                    </li>
-                    <li>
-                        <Link to="/orders">
-                            <div href="#" class="nav-link text-white">
-                            Orders
-                            </div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/products">
-                            <div href="#" class="nav-link text-white">
-                            Products
-                            </div>
-                        </Link>
-                    </li>
-                    </ul>
-                    <hr/>
-                    <div class="dropdown">
-                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/>
-                        <strong>mdo</strong>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
                     </div>
+                </nav>
+
+                {/* Movie Search Bar */}
+                <div className="search-bar">
+                    <input type="text" placeholder="Search for movies..." />
+                    <button onClick={this.handleSearch}>Search</button>
                 </div>
-                <div class="col-9">
-                    <Switch>
-                        <Route path="/dashboard" component={Dashboard} />
-                    </Switch>
-                </div>
+
+                <Switch>
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/home" component={Home} />
+                </Switch>
             </div>
         );
     }
