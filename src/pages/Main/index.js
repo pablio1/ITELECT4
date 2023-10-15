dcc350e291b01a53ad1a109e69165df1b04fcf80
User <ayingneil15@gmail.com>
2023-10-15 13:04:12 +0800

    name

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -75,7 +75,7 @@ export default class Main extends Component {
         <div class="dropdown">
           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" style={{marginLeft: '15px'}}/>
-            <strong>mdo</strong>
+            <strong>Neil</strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
             <li><a class="dropdown-item" href="#">New project...</a></li>
