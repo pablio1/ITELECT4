53596706b647e293617026dd4364d00393309264
User <ayingneil15@gmail.com>
2023-10-20 00:34:23 +0800

    sad

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './style.css'; // Import the CSS file
 
+// Import your images using relative paths
+import image1 from '../../piucturesmovie/movie.png';
+import image2 from '../../piucturesmovie/movie2.png';
+import image3 from '../../piucturesmovie/movie3.png';
+import image4 from '../../piucturesmovie/movie4.png';
+import image5 from '../../piucturesmovie/movie5.png';
+import image6 from '../../piucturesmovie/movie6.png';
+
 class Home extends Component {
     state = {
         searchQuery: '',
@@ -27,7 +35,25 @@ class Home extends Component {
                         onChange={this.handleSearchChange}
                         className="search-input" // Apply the CSS class to the input
                     />
-                    <button onClick={this.handleSearchSubmit} className="search-button">Search</button> {/* Apply the CSS class to the button */}
+                    <button onClick={this.handleSearchSubmit} className="search-button">
+                        Search
+                    </button> {/* Apply the CSS class to the button */}
+                </div>
+
+                {/* Display the images in a 3x2 grid */}
+                <div className="image-grid">
+                    <div className="image-row">
+                        <img src={image1} alt="Image 1" className="grid-image" />
+                        <img src={image2} alt="Image 2" className="grid-image" />
+                    </div>
+                    <div className="image-row">
+                        <img src={image3} alt="Image 3" className="grid-image" />
+                        <img src={image4} alt="Image 4" className="grid-image" />
+                    </div>
+                    <div className="image-row">
+                        <img src={image5} alt="Image 5" className="grid-image" />
+                        <img src={image6} alt="Image 6" className="grid-image" />
+                    </div>
                 </div>
             </div>
         );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -32,7 +32,7 @@ export default class Main extends Component {
             <Link to="/home" onClick={() => this.handleClickSidebar('home')} Link style={{color: 'inherit', textDecoration: 'inherit'}}>
             <div class={this.state.activePage === 'home' ? "nav-link active" : "text-white nav-link"} aria-current="page">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 2 16 16" style={{marginRight: '5px'}}>
-  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
 </svg>  Home
             </div>
             </Link>
@@ -41,8 +41,8 @@ export default class Main extends Component {
             <Link to="/dashboard" onClick={() => this.handleClickSidebar('dashboard')} Link style={{color: 'inherit', textDecoration: 'inherit'}}>
             <div class={this.state.activePage === 'dashboard' ? "nav-link active" : "text-white nav-link"}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 20" style={{marginRight: '5px'}}>
-  <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
-  <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+            <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
 </svg>    Dashboard
             </div>
             </Link>
@@ -51,8 +51,8 @@ export default class Main extends Component {
           <Link to="/Orders" onClick={() => this.handleClickSidebar('Orders')} Link style={{color: 'inherit', textDecoration: 'inherit'}}>
             <div class={this.state.activePage === 'Orders' ? "nav-link active" : "text-white nav-link"}>
             <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-calendar-check" viewBox="0 3 16 20" style={{marginRight: '7px'}}>
-  <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+            <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 </svg>    Orders
             </div>
             </Link>
@@ -84,9 +84,8 @@ export default class Main extends Component {
             <strong>Neil</strong>
           </a>
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
             <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
             <li><hr class="dropdown-divider"/></li>
             <li><a class="dropdown-item" href="#">Sign out</a></li>
           </ul>
