f4a490003c2047ce0ee8973c60cfbd5ddb0cf545
Tinapay,-Lance-Carvin-L <tinapaylance@gmail.com>
2023-10-21 13:42:12 +0800

    pogi eric

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main';
+//import Home from '.
 
 class App extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/login" component = {Login}  />
+                <Route path="/" component = {Main}  />
                 <Route path="/register" component = {Register}  />
                 <Route path="/Main" component = {Main} />
             </Switch>
