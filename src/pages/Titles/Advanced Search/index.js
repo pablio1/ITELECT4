ba9d406dc8bec0a58d64b593c24e82ed02b4a306
Mildred Amores <msamores000@gmail.com>
2023-10-26 19:29:25 +0800

    api

--- src/pages/Titles/Advanced Search/index.js
+++ src/pages/Titles/Advanced Search/index.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Search extends Component {
+    state = {title: '', results: []}
+    handleChangeInput = e =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    handleSearchButton = () =>{
+        const {title} = this.state;
+        const headers = {
+            'Access-Control-Allow-Origin': '*'
+        }
+        axios.get('https://api.consumet.org/light-novels/readlightnovels/'+title, headers)
+        .catch((e) => {
+            console.log(e);
+            return {status: 401, message: 'Unauthorized'}
+        })
+        .then((response) => {
+            this.setState({
+                result: response.data.results
+            })
+        })
+    }
     render() {
+
+            var books = this.state.results.length !==0 ? (
+                this.state.results.map((data, index) => {
+                    console.log(data);
+                    return (
+                        <div key ={index} class="card shadow-sm">
+                            <img class="bd-placeholder-img card-img-top" src={data.image} alt="Card image cap" width="100%" height="225"/>
+                            <div class="card body">
+                                <h5 class="card-title">{data.title}</h5>
+                                <button class="btn btn-primary">Read</button>
+                            </div>
+                        </div>
+                    )
+                })              
+            ) : "";
+
         return (
             <div>
                 <div href="/" class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none" style={{paddingTop: '20px'}}>
@@ -15,6 +54,7 @@ class Search extends Component {
 
                 <div style={{padding:'5px'}}>
                     <input type="search" class="form-control" placeholder="Search..." aria-label="Search"/>
+                    <button type="button" class="btn btn-primary">Search</button>
                 </div>
 
                 <div style={{padding: '10px 5px'}}>
