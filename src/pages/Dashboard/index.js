34a2604da962298cf09efdb0e2ecf3ccfb29a9d9
Beth <114622320+BobethDinoy@users.noreply.github.com>
2023-11-11 12:09:06 +0800

    bobit

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,59 +2,77 @@ import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 
 class Dashboard extends Component {
-    state = {searchAnime: '', lists: [], image: null};
-    handleSearch = e => {
-        if(e.key === "Enter"){
-            //API, data, header
-            var header  = {
-                "Access-Control-Allow-Origin": "*"
-            }
-            axios.get("https://api.consumet.org/manga/mangadex/"+this.state.searchAnime,header)
-            .then(response =>{
+    state = { searchAnime: '', lists: [] };
+
+    handleSearch = async (e) => {
+        if (e.key === 'Enter') {
+            try {
+                const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${this.state.searchAnime}`);
+                console.log('Manga API Response:', response.data);
+
+                // Assuming that the API response has a 'data' property containing an array of results
+                const mangaResults = response.data?.data || [];
+
                 this.setState({
-                    lists: response.data.results
-                })
-            })
+                    lists: mangaResults
+                });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     }
-    handleChangeInput = e =>{
+
+    componentDidMount() {
+        // Automatically load data when the component mounts
+        this.handleSearch({ key: 'Enter' });
+    }
+
+    handleChangeInput = (e) => {
         this.setState({
             [e.target.name]: e.target.value
-        })
+        });
     }
+
     render() {
-        //?:
         const { lists } = this.state;
-        var movieLists = lists.length !== 0 ? lists.map((data, index) => {
-            return (
-                <Fragment>
-                     <div className="row">
-                        <div className="col-4">
-                            <div class="card">
-                                <img class="card-img-top" src={data.image} alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">{data.title}</h5>
-                                    <div href="#" class="btn btn-primary">Watch</div>
-                                </div>
+        const mangaLists = lists.length !== 0 ? (
+            <div className="row">
+                {lists.map((data, index) => (
+                    <div className="col-4" key={index}>
+                        <div className="card">
+                            <img className="card-img-top" src={data.images.jpg.image_url} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">{data.title}</h5>
+                                <a href={data.url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                                    Read
+                                </a>
                             </div>
                         </div>
                     </div>
-                </Fragment>
-            )
-        }) : "Movie Not Found!";
+                ))}
+            </div>
+        ) : "Manga Not Found!";
 
         return (
             <Fragment>
                 <div className="row">
                     <div className='col-12'>
-                        <div class="form-group">
-                            <input name="searchAnime" value={this.state.searchAnime} onKeyDown={this.handleSearch} 
-                            onChange={this.handleChangeInput} type="email" class="form-control" 
-                            id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seach"/>
+                        <div className="form-group">
+                            <input
+                                name="searchAnime"
+                                value={this.state.searchAnime}
+                                onKeyDown={this.handleSearch}
+                                onChange={this.handleChangeInput}
+                                type="text"
+                                className="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                placeholder="Search"
+                            />
                         </div>
                     </div>
                 </div>
-               {movieLists}
+                {mangaLists}
             </Fragment>
         );
     }
