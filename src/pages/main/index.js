4f99556c8b7b7df01cd95776711b618af7021958
Rivera <billy100007@gmail.com>
2023-11-19 01:26:32 +0800

    small changes

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -67,6 +67,15 @@ class Main extends Component {
                             </div>
                         </Link>
                     </li>
+                    <li>
+                        <Link to="/register"onClick={() => this.handleClickSidebar('Register')}Link style={{color:'inherit',textDecoration:'inherit'}}>
+                        <div class={this.state.activePage === 'Register' ? "nav-link active" : "text-white nav-link" } >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box2" viewBox="0 0 16 16" style={{marginRight: '5px', marginBottom:'5px'}}>
+  <path d="M2.95.4a1 1 0 0 1 .8-.4h8.5a1 1 0 0 1 .8.4l2.85 3.8a.5.5 0 0 1 .1.3V15a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4.5a.5.5 0 0 1 .1-.3L2.95.4ZM7.5 1H3.75L1.5 4h6V1Zm1 0v3h6l-2.25-3H8.5ZM15 5H1v10h14V5Z"/>
+</svg>Register
+                            </div>
+                        </Link>
+                    </li>
                     </ul>
                     <hr/>
                     <div class="dropdown">
