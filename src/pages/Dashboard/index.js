9cf3f51895de03e18fb9336eee92b5a8b9528511
Archie480 <bontogarchie@gmail.com>
2023-11-18 09:21:35 +0800

    NOV 18

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,69 +1,120 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieData: null,
+      loading: false,
+      error: null,
+      searchTerm: localStorage.getItem('searchTerm') || '',
+    };
+  }
 
-    state = {searchAnime: '', lists: []};
-    handleSearch = e => {
-        if(e.key === "Enter"){
-            //API, data, header
-            var header = {
-                "Access-Control-Allow-Origin": "*"
-            }
-            axios.get("https://api.consumet.org/movies/viewasian/"+this.state.searchAnime,header)
-            .then(response =>{
-                this.setState({
-                    lists: response.data.results
-                })
-            })
-            .catch(error => {
-                console.error("Error fetching movie data:", error);
-            });
-        }
+  componentDidMount() {
+    this.fetchMovieData();
+  }
+
+  fetchMovieData = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const response = await axios.get(`https://api.consumet.org/movies/dramacool/${this.state.searchTerm}`);
+
+      console.log('API response:', response);
+
+      this.setState({ movieData: response.data.results, loading: false });
+    } 
+    catch (error) {
+      console.error('Error fetching movie data:', error);
+      this.setState({
+        error: `An error occurred while fetching movie data: ${error.message || 'Unknown error'}`,
+        loading: false,
+      });
     }
-    handleChangeInput = e =>{
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  };
 
-    render() {
-        
-        const { lists } = this.state;
-        var movieLists = lists.length !== 0? lists.map((data, index) => {
-            return (
-                <Fragment>
-                    <div className="row">
-                        <div className="col-4">
-                            <div class="card">
-                                <img class="card-img-top" src={data.image} alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">{data.title}</h5>
-                                    <div href="#" class="btn btn-primary">Watch</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Fragment>
-            )
-        }) : "Movie Not Found!"; 
+  handleSearchInputChange = (event) => {
+    const searchTerm = event.target.value;
+    this.setState({ searchTerm });
+    localStorage.setItem('searchTerm', searchTerm);
+  };
 
-        return (
-            <Fragment>
-                <div className="row">
-                    <div className="col-12">
-                        <div class="form-group">
-                            <input name="searchAnime" value={this.state.searchAnime} onKeyDown={this.handleSearch} 
-                            onChange={this.handleChangeInput} type="email" class="form-control"
-                            id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Search"
-                            />
-                        </div>
+  handleSearch = () => {
+    this.fetchMovieData();
+  };
+
+  render() {
+    const { movieData, loading, error, searchTerm } = this.state;
+
+
+    return (
+      <div className="m-3">
+        {}
+        <div>
+          <div className="d-flex flex-row">
+            <div className="col">
+              <input
+                type="search"
+                className="form-control rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+                value={searchTerm}
+                onChange={this.handleSearchInputChange}
+              />
+            </div>
+            <div className="col">
+              <button type="button" className="btn btn-primary" onClick={this.handleSearch}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+                </svg>
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {}
+        {loading && <p>Loading...</p>}
+
+        {}
+        {error && <p>Error: {error}</p>}
+
+        {}
+        <div>
+          {Array.isArray(movieData) && movieData.length > 0 ? (
+            <div className="row mt-3">
+              {movieData.map((movie) => (
+                <div key={movie.id} className="col-md-4 mb-3">
+                  <div style={{ width: '15rem' }}>
+                    <div className="card border border-dark">
+                      <img src={movie.image} alt={movie.title} />
                     </div>
+                    <div style={{ textDecoration: 'none' }}>
+                      <Link to={`/view/${movie.id}`}>
+                        <center>
+                          <b>
+                            <p>{movie.title}</p>
+                          </b>
+                        </center>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-            </Fragment>
-        );
-        
-    }
+              ))}
+            </div>
+          ) : (
+            <p></p>
+          )}
+        </div>
+
+        {}
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
