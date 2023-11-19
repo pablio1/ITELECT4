57bfcf71f8546244b3b53909d8d8fb69bfa0bf3d
King Andrei Caburnay <kingandrei.caburnay@gmail.com>
2023-11-20 01:28:10 +0800

    description

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main/Index';
+import desc from './pages/Dashboard/desc';
 
 
 class App extends Component {
     render() {
         return (
             <Switch>
+                <Route path="/view/:mal_id" component={desc} />
                 <Route path="/login" component = {Login}  />
                 <Route path="/register" component = {Register}  />
                 <Route path="/" component = {Main} />
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
-
+import desc from './desc';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -9,7 +11,8 @@ class Dashboard extends Component {
       animeData: null,
       loading: false,
       error: null,
-      searchTerm: '', // New state for the search term
+      searchTerm: localStorage.getItem('searchTerm') || '', // Retrieve from local storage
+      selectedManga: null,
     };
   }
 
@@ -21,32 +24,39 @@ class Dashboard extends Component {
   fetchAnimeData = async () => {
     // Set loading to true before making the API request
     this.setState({ loading: true, error: null });
-
+  
     try {
       // Make the API request to Jikan for top anime
       const response = await axios.get(`https://api.jikan.moe/v4/manga?q=${this.state.searchTerm}`);
-
+  
       // Log the response to the console
-      console.log('API response:', response)
-
+      console.log('API response:', response);
+      
+  
       // Update the state with the fetched anime data
       this.setState({ animeData: response.data, loading: false });
     } catch (error) {
+
       // Handle errors and update the state with the error message
       console.error('Error fetching anime data:', error);
       this.setState({ error: 'An error occurred while fetching anime data', loading: false });
     }
   };
   
+
   handleSearchInputChange = (event) => {
-    this.setState({ searchTerm: event.target.value });
+    const searchTerm = event.target.value;
+
+    // Update state and local storage
+    this.setState({ searchTerm });
+    localStorage.setItem('searchTerm', searchTerm);
   };
 
   handleSearch = () => {
     // Call fetchAnimeData when the search button is clicked
+    localStorage.setItem('searchTerm', this.state.searchTerm);
     this.fetchAnimeData();
   };
-  
 
   render() {
     const { animeData, loading, error, searchTerm } = this.state;
@@ -87,20 +97,32 @@ class Dashboard extends Component {
         <div>
           {animeData && (
             <div className="row mt-3">
-            {animeData.data.map((anime) => (
-              <div key={anime.mal_id} className="col-md-4 mb-3">
-                <div style={{ width: '15rem'}}>
-                  <div className="card border border-dark">
-                  <img src={anime.images.jpg.image_url} alt={anime.title} />
+              {animeData.data.map((anime) => (
+                <div key={anime.mal_id} className="col-md-4 mb-3">
+                  <div style={{ width: '15rem' }}>
+                    <div className="card border border-dark">
+                      <img src={anime.images.jpg.image_url} alt={anime.title} />
+                    </div>
+                    <div style={{ textDecoration: 'none' }}>
+                    <Link to={`/view/${anime.mal_id}`}>
+                      <center>
+                        <b>
+                          <p>{anime.title}</p>
+                        </b>
+                      </center>
+                    </Link>
+                    </div>
                   </div>
-                  <center><b><p>{anime.title}</p></b></center>
                 </div>
-                
-              </div>
-            ))}
+              ))}
             </div>
           )}
         </div>
+
+        
+        
+
+
       </div>
     );
   }
