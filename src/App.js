87728cb08f73b4608675871d80166f9f28e242f7
Mia May Opaon <miamayopaon27@gmail.com>
2023-12-15 22:44:51 +0800

    Description

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main';
+import Home from './pages/Home';
+import MovieBox from './MovieBox';
+import { Navbar,Container,Nav,Form, FormControl,Button } from 'react-bootstrap';
 
-class App extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route path="/login" component = {Login}  />
-                <Route path="/register" component = {Register}  />
-                <Route path="/" component = {Main} />
-            </Switch>
-        );
+const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=bcc4ff10c2939665232d75d8bf0ec093";
+const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=<<api_key_here>>&query";
+function App() {
+    const [movies, setMovies]=useState([]);
+    const [query, setQuery]=useState('');
+    useEffect(() => {
+        fetch(API_URL)
+        .then((res)=>res.json())
+        .then(data=>{
+        console.log(data);
+        setMovies(data.results);
+        })
+    },[])
 
+    const searchMovie = async(e)=>{
+        e.preventDefault();
+        console.log("Searching");
+        try{
+          const url=`https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+          const res= await fetch(url);
+          const data= await res.json();
+          console.log(data);
+          setMovies(data.results);
+        }
+        catch(e){
+          console.log(e);
+        }
+      }
+    
+      const changeHandler=(e)=>{
+        setQuery(e.target.value);
+      }
+    return (
+        <>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container fluid>
+            <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
+            <Navbar.Brand href="/home">Trending</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+    
+              <Navbar.Collapse id="nabarScroll">
+                <Nav 
+                className="me-auto my-2 my-lg-3"
+                style={{maxHeight:'100px'}}
+                navbarScroll></Nav>
+    
+                <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+                  <FormControl
+                  type="search"
+                  placeholder="Movie Search"
+                  className="me-2"
+                  aria-label="search"
+                  name="query"
+                  value={query} onChange={changeHandler}></FormControl>
+                  <Button variant="secondary" type="submit">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+          {movies.length > 0 ?(
+            <div className="container">
+            <div className="grid">
+              {movies.map((movieReq)=>
+              <MovieBox key={movieReq.id} {...movieReq}/>)}
+                </div>
+        </div>
+          ):(
+            <h2>Sorry !! No Movies Found</h2>
+          )}
+        </div>   
+        </>
+       
+      );
     }
-}
+    
+
 
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,107 +1,61 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 class Home extends Component {
+    state = {searchAnime: "", lists: []};
+    handleSearch = e => {
+        if(e.key === "Enter"){
+            var header = {
+                "Access-Control-Allow-Origin": "*"
+            }
+            axios.get("https://api.consumet.org/movies/viewasian/"+this.state.searchAnime,header)
+            .then(response =>{
+                this.setState({
+                    lists: response.data.results
+                })
+                
+            
+            })
+        }
+    }
+
+    handleChangeInput = e =>{
+        this.setState({
+            [e.target.name]: e.target.value 
+        })
+       
+    }
+     
     render() {
-        return (
-            <div>
-                <div class="album py-5 bg-dark">
-                    <div class="container">
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="The Burial.JPG" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="13" style={{textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="14">The Burial</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="15">The Burial</text></svg>
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="16" style={{ textAlign: 'left'}}>The Burial</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="17" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="19" style={{ textAlign: 'left'}}></small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="20" style={{ textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="21">Expend4bles</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="22">Expend4bles</text></svg>
-
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="23" style={{ textAlign: 'left'}}>Expend4bles</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="24" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="26" style={{ textAlign: 'left'}}> </small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="27" style={{textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="28">The Nun II</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="29">The Nun II</text></svg>
-
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="30" style={{ textAlign: 'left'}}>The Nun II</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="31" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="33" style={{ textAlign: 'left'}}> </small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="34" style={{ textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="35">Barbie</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="36">Barbie</text></svg>
-
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="37" style={{ textAlign: 'left'}}>Barbie</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="38" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="40" style={{ textAlign: 'left'}}></small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="41" style={{ textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="42">Ballerina</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="43">Ballerina</text></svg>
-
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="44" style={{ textAlign: 'left'}}>Ballerina</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="45" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="47" style={{ textAlign: 'left'}}></small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                            <div class="col">
-                            <div class="card shadow-sm">
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="4468347" _mstHash="48" style={{ textAlign: 'left'}}><title _mstTextHash="177515" _mstHash="49">Blue Beetle</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="40%" y="50%" fill="#eceeef" dy=".3em" _mstTextHash="134056" _mstHash="50">Blue Beetle</text></svg>
-
-                                <div class="card-body">
-                                <p class="card-text" _msttexthash="9321065" _msthash="51" style={{ textAlign: 'left'}}>Blue Beetle</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" _msttexthash="63401" _msthash="52" style={{}}>PLAY</button>
-                                    </div>
-                                    <small class="text-muted" _msttexthash="115934" _msthash="54" style={{ textAlign: 'left'}}></small>
-                                </div>
-                                </div>
-                            </div>
-                            </div>
+        const { lists } = this.state;
+        var movieLists = lists.length !== 0 ? lists.map((data, index) => {
+            return (
+                <Fragment>
+                     <div className="row">
+                    <div className="col-8">
+                        <div class="card">
+                        <img class="card-img-top" src={data.image} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">{data.title}</h5>
+                            <div href="#" class="btn btn-primary">Watch Now</div>
+                        </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            
+                </Fragment>
+            )
+        }) : this.state.searchAnime !== "" ? "Movie not found!":""; 
+
+        return (
+            <Fragment>
+                <div className="row">
+                    <div className='col-12'>
+                        <div class="form-group">
+                            <input name= "searchAnime" value={this.state.searchAnime} onKeyDown={this.handleSearch} onChange={this.handleChangeInput} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Type to search" />
+                        </div>
+                    </div>    
+                </div>
+               {movieLists}
+            </Fragment>
         );
     }
 }
