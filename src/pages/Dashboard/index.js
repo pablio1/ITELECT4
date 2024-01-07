f5e12d3f6f3427721ef2fb0b8205d4aaf9ec5a35
User <ayingneil15@gmail.com>
2024-01-08 00:38:44 +0800

    1/8/24

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import './style.css'; // Import the CSS file
 
 class Dashboard extends Component {
     state = {searchAnime: '', lists: []};
