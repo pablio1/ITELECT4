baf8a54be7ec7451a720b7964565a065b9def7c5
User <ayingneil15@gmail.com>
2024-01-08 00:56:49 +0800

    1/8/24

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,37 +9,49 @@ import movieImage5 from './picturesmovie/movie5.PNG';
 import movieImage6 from './picturesmovie/movie6.PNG';
 
 const movieData = [
-  {
-    title: "Warcraft",
-    imageUrl: movieImage1,
-    overview: "A movie about the world of Azeroth and the conflict between the Horde and the Alliance.",
-  },
-  {
-    title: "Warcraft (2016)",
-    imageUrl: movieImage2,
-    overview: "Based on the video game series, the film portrays the initial encounters between the humans and the orcs.",
-  },
-  {
-    title: "Dota Dragonblood",
-    imageUrl: movieImage3,
-    overview: "An animated series based on the Dota 2 universe, exploring the story of the Dragon Knight, Davion.",
-  },
-  {
-    title: "World of Warcraft Dragonflight (2022)",
-    imageUrl: movieImage4,
-    overview: "A cinematic experience diving into the fantastical world of World of Warcraft.",
-  },
-  {
-    title: "World of Warcraft Fantasy",
-    imageUrl: movieImage5,
-    overview: "Immerse yourself in the fantasy realm of World of Warcraft with this epic cinematic journey.",
-  },
-  {
-    title: "War of Warcraft Cinematic Movie",
-    imageUrl: movieImage6,
-    overview: "Experience the intense battles and rich lore of the War of Warcraft in this cinematic masterpiece.",
-  },
-];
+    {
+      title: "Warcraft",
+      imageUrl: movieImage1,
+      overview: "A movie about the world of Azeroth and the conflict between the Horde and the Alliance.",
+      link: "https://www.bilibili.tv/en/video/2049633738?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Fantasy, Action",
+    },
+    {
+      title: "Warcraft (2016)",
+      imageUrl: movieImage2,
+      overview: "Based on the video game series, the film portrays the initial encounters between the humans and the orcs.",
+      link: "https://www.bilibili.tv/en/video/2005467934?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Fantasy, Action",
+    },
+    {
+      title: "Dota Dragonblood",
+      imageUrl: movieImage3,
+      overview: "An animated series based on the Dota 2 universe, exploring the story of the Dragon Knight, Davion.",
+      link: "https://www.bilibili.tv/en/video/2001434336?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Animation, Fantasy",
+    },
+    {
+      title: "World of Warcraft Dragonflight (2022)",
+      imageUrl: movieImage4,
+      overview: "A cinematic experience diving into the fantastical world of World of Warcraft.",
+      link: "https://www.bilibili.tv/en/video/2048942975?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Fantasy, Action",
+    },
+    {
+      title: "World of Warcraft Fantasy",
+      imageUrl: movieImage5,
+      overview: "Immerse yourself in the fantasy realm of World of Warcraft with this epic cinematic journey.",
+      link: "https://www.bilibili.tv/en/video/4787313554364928?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Fantasy, Action",
+    },
+    {
+      title: "War of Warcraft Cinematic Movie",
+      imageUrl: movieImage6,
+      overview: "Experience the intense battles and rich lore of the War of Warcraft in this cinematic masterpiece.",
+      link: "https://www.bilibili.tv/en/video/4787276243801600?bstar_from=bstar-web.homepage.recommend.all",
+      genre: "Fantasy, Action",
+    },
+  ];
 
 class Home extends Component {
   state = {
@@ -79,10 +91,15 @@ class Home extends Component {
         <div className="image-grid">
           {filteredMovies.map((movie, index) => (
             <div key={index} className="grid-item">
-              <img src={movie.imageUrl} alt={movie.title} className="grid-image" />
+              <a href={movie.link} target="_blank" rel="noopener noreferrer">
+                {/* Use anchor tag to make the image clickable */}
+                <img src={movie.imageUrl} alt={movie.title} className="grid-image" />
+              </a>
               <div className="image-caption">
                 <h3>{movie.title}</h3>
+                <p><strong>Genre:</strong> {movie.genre}</p>
                 <p>{movie.overview}</p>
+                
               </div>
             </div>
           ))}
