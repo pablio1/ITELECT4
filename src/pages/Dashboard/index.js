f4386dc4dc45e7d1a3b795b73dedb43f459232fc
Rivera <billy100007@gmail.com>
2024-01-09 11:39:26 +0800

    Working

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,95 +1,122 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import '../Dashboard/main.css'; 
 
 class Dashboard extends Component {
-    state = { title: '', results: [] };
+  constructor(props) {
+    super(props);
+    this.state = {
+      animeData: null,
+      loading: false,
+      error: null,
+      searchTerm: localStorage.getItem('searchTerm') || '',
+    };
+  }
 
-    handleChangeInput = (e) => {
-        this.setState({ 
-            [e.target.name]: e.target.value
-        });
+
+  fetchAnimeData = async () => {
+    
+    this.setState({ loading: true, error: null });
+
+    try {
+      
+      const response = await axios.get(`https://kitsu.io/api/edge/manga?filter[text]=${this.state.searchTerm}`);
+
+      
+      console.log('API response:', response);
+
+      
+      this.setState({ animeData: response.data.data, loading: false });
+    } catch (error) {
+      
+      console.error('Error fetching anime data:', error);
+      this.setState({ error: 'An error occurred while fetching anime data', loading: false });
     }
+  };
 
-    handleSearchButton = () => {
-        const { title } = this.state;
-        const headers = {
-            'Access-Control-Allow-Origin': '*',
-        };
-        axios.get('' + title, headers)
-            .catch((e) => {
-                console.log(e);
-                return { status: 401, message: 'Unauthorized' };
-            })
-            .then((response) => {
-                this.setState({
-                    results: response.data.results,
-                });
-            });
-    }
+  componentDidMount() {
+    
+    this.fetchAnimeData();
+  }
 
-    render() {
-        var books = this.state.results.length !== 0 ? (
-            this.state.results.map((data, index) => {
-                console.log(data);
-                return (
-                    <div key={index} className="card" style={{ width: '18rem' }}>
-                        <img className="card-img-top" src={data.image} alt="Card image cap" />
-                        <div className="card-body">
-                            <h5 className="card-title">{data.title}</h5>
-                            <button className="btn btn-primary">Read</button>
-                        </div>
+  handleSearchInputChange = (event) => {
+    const searchTerm = event.target.value;
+
+    
+    this.setState({ searchTerm });
+    localStorage.setItem('searchTerm', searchTerm);
+  };
+
+  handleSearch = () => {
+    
+    localStorage.setItem('searchTerm', this.state.searchTerm);
+    this.fetchAnimeData();
+  };
+
+  render() {
+    const { animeData, loading, error, searchTerm } = this.state;
+    return (
+      <div className="m-3">
+        {/* Search Bar */}
+        <div className="d-flex justify-content-center">
+  <div className="col-md-6">
+    <div className="d-flex flex-row">
+      <div className="col">
+        <input
+          type="search"
+          className="form-control rounded"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+          value={searchTerm}
+          onChange={this.handleSearchInputChange}
+        />
+      </div>
+      <div className="col">
+        <button type="button" className="btn btn-primary" onClick={this.handleSearch}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+          </svg>
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+        {/* Display loading message while anime data is being fetched */}
+        {loading && <p>Loading...</p>}
+
+        {/* Display error message if an error occurred */}
+        {error && <p>Error: {error}</p>}
+
+        {/* Display the fetched anime data if available */}
+        <div>
+          {animeData && (
+            <div className="row mt-3 justify-content-center">
+              {animeData.map((anime) => (
+                <div key={anime.id} className="col-md-4 mb-3">
+                  <div style={{ width: '15rem' }}>
+                    <div className="card border border-dark">
+                      <img src={anime.attributes.posterImage.small} alt={anime.attributes.titles.en} />
                     </div>
-                );
-            })
-        ) : "";
-
-        return (
-            <div className="light-brown-background">
-                <title>DASHBOARD PAGE</title>
-                <center><h1>DASHBOARD PAGE</h1></center> 
-                <center>
-                    <a href='/Login' target='blank'>
-                    <button className="btn btn-secondary custom-button">
-                            <h1 className='address'>LOG IN PAGE</h1>
-                        </button>
-                    </a>
-                </center>
-
-                <div>
-                    <br></br>
-                    <center><h1>MANWHHHHAAA!!!</h1></center>  
-
-                    <center>
-                        <div className='bottom'>
-                            <div className="m-3 mt-5">
-                                <div className="row justify-content-center">
-                                    <div className="col-lg-9 col-md-3 col-sm-12 p-0 text-center">
-                                        <input type="text" className="form-control search-slt" value={this.state.title} name="title" onChange={this.handleChangeInput} placeholder="Search" />
-                                    </div>
-                                    <div className="col-lg-3 col-md-3 col-sm-12 p-0">
-                                        <button type="button" className="btn btn-outline-success" onClick={this.handleSearchButton}>Search</button>
-                                    </div>
-                                </div>
-                                <div className='main light-blue-background'>
-                                    <div className='left'><h1>left</h1></div>
-                                    <div className='right'><h1>RIGHT</h1></div>
-                                    <div className='middle'><h1>middle</h1></div>
-                                </div>
-                                <div className="row mt-5">
-                                    {books}
-                                </div>
-                            </div>
-                        </div>
-                    </center>
+                    <div style={{ textDecoration: 'none' }}>
+                    <Link to={`/view/${anime.id}`}>
+                      <center>
+                        <b>
+                          <p>{anime.attributes.titles.en}</p>
+                        </b>
+                      </center>
+                    </Link>
+                    </div>
+                  </div>
                 </div>
+              ))}
             </div>
-        );
-    }
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
-
-
-
-//https://api.consumet.org/light-novels/readlightnovels/
